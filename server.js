40751c47f250491906237c80d39c72d99247c2de
Michael Lazarski <michael@lazarski.me>
2015-02-13 14:12:01 +0100

    init commit

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var Fiber = Npm.require('fibers');
 Paypal.payment = paypal_sdk.payment;
 Paypal.credit_card = paypal_sdk.credit_card;
 
-Paypal.setConfig = function(config){
+Paypal.setConfig = function(config) {
   paypal_sdk.configure(config);
-  paypal_sdk.generate_token(function(error, token){
-    if(error){
+  paypal_sdk.generate_token(function(error, token) {
+    if (error) {
       // console.error("generate Token error: " + error);
     } else {
       console.log("token: " + token);
@@ -16,9 +16,11 @@ Paypal.setConfig = function(config){
 };
 
 Meteor.methods({
-  "PayPalPaymentList": function(){
-    paypal_sdk.payment.list({ "count": 10 }, function(error, payment_history){
-      if(error){
+  "PayPalPaymentList": function() {
+    paypal_sdk.payment.list({
+      "count": 10
+    }, function(error, payment_history) {
+      if (error) {
         console.log("error: " + error);
         return error;
       } else {
@@ -28,7 +30,7 @@ Meteor.methods({
     });
     return false;
   },
-  "payPalCreatePayment": function(data){
+  "payPalCreatePayment": function(data) {
     var payment_details = {
       "intent": "sale",
       "payer": {
@@ -50,7 +52,7 @@ Meteor.methods({
         },
         "amount": {
           "currency": "EUR",
-          "total": parseInt(data[0])*parseInt(data[1])
+          "total": parseInt(data[0]) * parseInt(data[1])
         },
         "description": "This is the payment description."
       }]
@@ -58,42 +60,46 @@ Meteor.methods({
     var bookingId = data[4];
     // var booking = Booking.findOne(data[5]);
     var fut = new Future();
-    paypal_sdk.payment.create(payment_details, function(error, payment){
-      if(error){
+    paypal_sdk.payment.create(payment_details, function(error, payment) {
+      if (error) {
         fut.return(error);
       } else {
-        console.log("else:" , data[4]);
-        var bookingUpdate = Fiber(function (bookingId) {
+        console.log("else:", data[4]);
+        var bookingUpdate = Fiber(function(bookingId) {
           console.log(bookingId);
-          Booking.update(bookingId ,{$set : {"payment": payment}}, function(error) {
-            if(error)
-              console.log(error);
-            });
-          });
-          bookingUpdate.run(bookingId);
-          for (var i = 0; i < payment.links.length; i++) {
-            if (payment.links[i].rel === 'approval_url') {
-              fut.return(payment.links[i].href);
+          Booking.update(bookingId, {
+            $set: {
+              "payment": payment
             }
+          }, function(error) {
+            if (error)
+              console.log(error);
+          });
+        });
+        bookingUpdate.run(bookingId);
+        for (var i = 0; i < payment.links.length; i++) {
+          if (payment.links[i].rel === 'approval_url') {
+            fut.return(payment.links[i].href);
           }
         }
-      });
-      return fut.wait();
-    },
-    "payPalExecutePayment": function(paymentId, paymentDetails){
-      var fut = new Future();
-      paypal_sdk.payment.execute(paymentId, paymentDetails.payer_id, function(error, payment){
-        if(error){
-          console.log(error);
-          fut.return("something went Wrong please Retry!");
-        }else{
-          if(payment.state === 'approved'){
-            fut.return('approved');
-          }else{
-            fut.return('Not approved');
-          }
+      }
+    });
+    return fut.wait();
+  },
+  "payPalExecutePayment": function(paymentId, paymentDetails) {
+    var fut = new Future();
+    paypal_sdk.payment.execute(paymentId, paymentDetails.payer_id, function(error, payment) {
+      if (error) {
+        console.log(error);
+        fut.return("something went Wrong please Retry!");
+      } else {
+        if (payment.state === 'approved') {
+          fut.return('approved');
+        } else {
+          fut.return('Not approved');
         }
-      });
-      return fut.wait();
-    }
-  });
+      }
+    });
+    return fut.wait();
+  }
+});
